108905ab384593c60746df3e1aa602283fbcc8ed
whgek <dugoddlwhgek@gmail.com>
2025-03-30 21:47:19 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './data.js',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
